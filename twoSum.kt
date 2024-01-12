e5d3aeb76a9af37c1bc9af8745d39c2bfdb8daf3
aldojunior990 <aldojunior990@gmail.com>
2024-01-12 11:20:09 -0300

    first commit

---
+++ twoSum.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map: MutableMap<Int, Int> = mutableMapOf()
+        for (i in nums.indices) {
+            val item = nums[i]
+            val complemento = target - item
+            if (map.containsValue(complemento)) {
+                return intArrayOf(map.values.indexOf(complemento), i)
+            }
+            map[i] = item
+        }
+        return intArrayOf()
+    }
+}
+
+
+fun main() {
+    val twoSum = Solution()
+    val nums = intArrayOf(0, 4, 3, 0)
+    val target = 0
+    val res = twoSum.twoSum(nums, target)
+    println(res.toList().toString())
+}
